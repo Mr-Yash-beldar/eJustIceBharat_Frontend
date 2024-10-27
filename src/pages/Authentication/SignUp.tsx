7dc82724f0fb5052271ac6848bf866edc28dbc5e
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-10-28 01:06:21 +0530

    added signup api

--- src/pages/Authentication/SignUp.tsx
+++ src/pages/Authentication/SignUp.tsx
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import LogoDark from '../../images/logo/logo_light.png';
 import Logo from '../../images/logo/logo_dark.png';
+import axiosInstance from '../../utils/axiosInstance';
+import { AxiosError } from 'axios';
 
 const SignUp: React.FC = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    mobile: '',
+    litigant_name: '',
+    litigant_email: '',
+    litigant_password: '',
+    litigant_mob: '',
+    litigant_confirm_password: '',
   });
 
   const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,28 +26,42 @@ const SignUp: React.FC = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     setLoading(true); // Enable loading
 
-    // Simulate an asynchronous operation (e.g., API call) with setTimeout
-    setTimeout(() => {
-      const data = {
-        name: formData.name,
-        email: formData.email,
-        password: formData.password,
-        mobile: formData.mobile,
-      };
+    try {
+      console.log('Form data:', formData);
+      const response = await axiosInstance.post('/litigants/signup', formData);
+      console.log('Sign up successful:', response.data);
+      const userId = response.data.id; // Access user ID from the response
 
-      // Log the form data
-      console.log('Saved data:', data);
-
-      // Disable loading after the operation is complete
-      setLoading(false);
-
-      // Redirect to the Verify Email page
-      navigate('/auth/VerifyEmail'); // Replace with the actual route
-    }, 2000);
+      // Send OTP to the user's email
+      const otpResponse = await axiosInstance.get(
+        `/email/sendOtp?id=${userId}`,
+      );
+      if (otpResponse.status === 200) {
+        navigate('/auth/VerifyEmail/${userId}'); // Redirect to the email verification page
+      } else {
+        alert(otpResponse.data.error);
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+      if (error.response && error.response.status === 409) {
+        // User already exists, handle accordingly
+        alert(error.response.data.error); // Show alert with the error message
+        navigate('/auth/signin'); // Redirect to the signin page
+      } else {
+        alert(error); // General error alert
+      }
+    }
+    else {
+      // Handle non-Axios errors if needed
+      alert(`Unexpected error: ${error}`);
+    }
+    } finally {
+      setLoading(false); // Disable loading
+    }
   };
 
   return (
@@ -201,9 +218,9 @@ const SignUp: React.FC = () => {
                       type="text"
                       placeholder="Enter your full name"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                      name="name"
+                      name="litigant_name"
                       onChange={inputChangeHandler}
-                      value={formData.name}
+                      value={formData.litigant_name}
                     />
 
                     <span className="absolute right-4 top-4">
@@ -239,9 +256,9 @@ const SignUp: React.FC = () => {
                       type="email"
                       placeholder="Enter your email"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                      name="email"
+                      name="litigant_email"
                       onChange={inputChangeHandler}
-                      value={formData.email}
+                      value={formData.litigant_email}
                     />
 
                     <span className="absolute right-4 top-4">
@@ -266,6 +283,38 @@ const SignUp: React.FC = () => {
 
                 <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-black dark:text-white">
+                    Mobile No.
+                  </label>
+                  <div className="relative">
+                    <input
+                      type="text"
+                      placeholder="Enter your Mobile Number"
+                      className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      name="litigant_mob"
+                      onChange={inputChangeHandler}
+                      value={formData.litigant_mob}
+                    />
+
+                    <span className="absolute right-4 top-4">
+                      <svg
+                        className="fill-current"
+                        width="22"
+                        height="22"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M17.707 12.293A1 1 0 0118 13v3a3 3 0 01-3 3c-5.523 0-10-4.477-10-10a3 3 0 013-3h3a1 1 0 01.707.293l2 2a1 1 0 01-.037 1.392l-1.457 1.364a1 1 0 00-.263.999c.35 1.052 1.466 2.168 2.518 2.518a1 1 0 00.999-.263l1.364-1.457a1 1 0 011.392-.037l2 2z"
+                          fill="#D1D5DB"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+
+                <div className="mb-4">
+                  <label className="mb-2.5 block font-medium text-black dark:text-white">
                     Password
                   </label>
                   <div className="relative">
@@ -273,9 +322,9 @@ const SignUp: React.FC = () => {
                       type="password"
                       placeholder="Enter your password"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                      name="password"
+                      name="litigant_password"
                       onChange={inputChangeHandler}
-                      value={formData.password}
+                      value={formData.litigant_password}
                     />
 
                     <span className="absolute right-4 top-4">
@@ -304,16 +353,16 @@ const SignUp: React.FC = () => {
 
                 <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-black dark:text-white">
-                    Mobile No.
+                    Password
                   </label>
                   <div className="relative">
                     <input
-                      type="text"
-                      placeholder="Enter your Mobile Number"
+                      type="password"
+                      placeholder="Enter your password"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                      name="mobile"
+                      name="litigant_confirm_password"
                       onChange={inputChangeHandler}
-                      value={formData.mobile}
+                      value={formData.litigant_confirm_password}
                     />
 
                     <span className="absolute right-4 top-4">
@@ -321,14 +370,20 @@ const SignUp: React.FC = () => {
                         className="fill-current"
                         width="22"
                         height="22"
-                        viewBox="0 0 24 24"
+                        viewBox="0 0 22 22"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
                       >
-                        <path
-                          d="M17.707 12.293A1 1 0 0118 13v3a3 3 0 01-3 3c-5.523 0-10-4.477-10-10a3 3 0 013-3h3a1 1 0 01.707.293l2 2a1 1 0 01-.037 1.392l-1.457 1.364a1 1 0 00-.263.999c.35 1.052 1.466 2.168 2.518 2.518a1 1 0 00.999-.263l1.364-1.457a1 1 0 011.392-.037l2 2z"
-                          fill="#D1D5DB"
-                        />
+                        <g opacity="0.5">
+                          <path
+                            d="M16.1547 6.80626V5.91251C16.1547 3.16251 14.0922 0.825009 11.4797 0.618759C10.0359 0.481259 8.59219 0.996884 7.52656 1.95938C6.46094 2.92188 5.84219 4.29688 5.84219 5.70626V6.80626C3.84844 7.18438 2.33594 8.93751 2.33594 11.0688V17.2906C2.33594 19.5594 4.19219 21.3813 6.42656 21.3813H15.5016C17.7703 21.3813 19.6266 19.525 19.6266 17.2563V11C19.6609 8.93751 18.1484 7.21876 16.1547 6.80626ZM8.55781 3.09376C9.31406 2.40626 10.3109 2.06251 11.3422 2.16563C13.1641 2.33751 14.6078 3.98751 14.6078 5.91251V6.70313H7.38906V5.67188C7.38906 4.70938 7.80156 3.78126 8.55781 3.09376ZM18.1141 17.2906C18.1141 18.7 16.9453 19.8688 15.5359 19.8688H6.46094C5.05156 19.8688 3.91719 18.7344 3.91719 17.325V11.0688C3.91719 9.52189 5.15469 8.28438 6.70156 8.28438H15.2953C16.8422 8.28438 18.1141 9.52188 18.1141 11V17.2906Z"
+                            fill=""
+                          />
+                          <path
+                            d="M10.9977 11.8594C10.5852 11.8594 10.207 12.2031 10.207 12.65V16.2594C10.207 16.6719 10.5508 17.05 10.9977 17.05C11.4102 17.05 11.7883 16.7063 11.7883 16.2594V12.6156C11.7883 12.2031 11.4102 11.8594 10.9977 11.8594Z"
+                            fill=""
+                          />
+                        </g>
                       </svg>
                     </span>
                   </div>
