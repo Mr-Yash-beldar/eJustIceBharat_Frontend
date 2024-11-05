83db547b535ffac213f36ff954fe778ef5a73619
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-11-05 10:45:52 +0530

    added role of authenticated

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,82 +1,107 @@
-import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from 'react';
 import axiosInstance from '../utils/axiosInstance';
+import { toast } from 'react-toastify';
 
 interface AuthContextType {
-    isAuthenticated: boolean;
-    profileCompleted: boolean;
-    profileCompletionPercentage: number;
-    loading: boolean;
-    setIsAuthenticated: (value: boolean) => void;
-    logout: () => void;
+  isAuthenticated: boolean;
+  profileCompleted: boolean;
+  profileCompletionPercentage: number;
+  loading: boolean;
+  role: string ;
+  setIsAuthenticated: (value: boolean) => void;
+  setRole: (value: string ) => void;
+  logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 interface AuthProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [profileCompleted, setProfileCompleted] = useState(false);
-    const [profileCompletionPercentage, setProfileCompletionPercentage] = useState(0);
-    const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [profileCompleted, setProfileCompleted] = useState(false);
+  const [profileCompletionPercentage, setProfileCompletionPercentage] =
+    useState(0);
+  const [loading, setLoading] = useState(true);
+  const [role, setRole] = useState<string>('litigant');
 
-    const verifyToken = async () => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            try {
-                const { data } = await axiosInstance.get('/auth/verifyLitigant-token', {
-                    headers: { Authorization: `Bearer ${token}` }
-                });
-                if (data.valid) {
-                    setIsAuthenticated(true);
-                    
-                    const profileResponse = await axiosInstance.get('/litigants/getDetails', {
-                        headers: { Authorization: `Bearer ${token}` }
-                    });
+  const verifyToken = async () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const { data } = await axiosInstance.get('/auth/verifyLitigant-token', {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        
+        if (data.valid) {
+          setIsAuthenticated(true);
+          setRole(data.litigant.role || 'litigant');
 
-                    const { isCompleted, completionPercentage } = profileResponse.data;
-                    setProfileCompleted(isCompleted);
-                    setProfileCompletionPercentage(completionPercentage);
-                } else {
-                    setIsAuthenticated(false);
-                  
-                }
-            } catch (error) {
-                console.error('Error verifying token:', error);
-                setIsAuthenticated(false);
-             
-            }
+          const profileResponse = await axiosInstance.get(
+            '/litigants/getDetails',
+            {
+              headers: { Authorization: `Bearer ${token}` },
+            },
+          );
+
+          const { isCompleted, completionPercentage } = profileResponse.data;
+          setProfileCompleted(isCompleted);
+          setProfileCompletionPercentage(completionPercentage);
         } else {
-            console.log('No token found');
-            setIsAuthenticated(false);
+          setIsAuthenticated(false);
         }
-
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        verifyToken();
-    }, []);
-
-    const logout = () => {
+      } catch (error: any) {
+        toast.error('Error verifying token:', error);
         setIsAuthenticated(false);
-        localStorage.removeItem('token');
-        localStorage.setItem("isAuthenticated", "false");
-    };
+      }
+    } else {
+      toast.error('No token found');
+      setIsAuthenticated(false);
+    }
 
-    return (
-        <AuthContext.Provider value={{ isAuthenticated,setIsAuthenticated, profileCompleted, profileCompletionPercentage, loading, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    verifyToken();
+  }, []);
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    localStorage.removeItem('token');
+    localStorage.setItem('isAuthenticated', 'false');
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        profileCompleted,
+        profileCompletionPercentage,
+        loading,
+        logout,
+        role,
+        setRole,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = (): AuthContextType => {
-    const context = useContext(AuthContext);
-    if (context === undefined) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
 };
